061dc491938f206956580878663d894ac43caf3f
Seyitname <seyit@example.com>
2025-12-22 16:50:47 +0300

    jbm

--- obj/Debug/net8.0/SeyitnameWebSite.AssemblyInfo.cs
+++ obj/Debug/net8.0/SeyitnameWebSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeyitnameWebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df5b3bfb3bcfa7d0a886b838ba8afdbfa25ddf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96cdbc5a33088e77b81640e08fb7255d8c6f80d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeyitnameWebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeyitnameWebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
