96dd0b57fda2223fc488b1d17c4bcf5d7dadc1fc
Seyitname <seyit@example.com>
2026-01-03 22:14:35 +0300

    hgf

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SeyitnameWebSite.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
